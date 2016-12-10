0776e6b9a0396969759b3cc4b248241ceb1d894a
aaronkaltman <aaronkaltman@gmail.com>
2016-12-10 11:50:29 -0500

    not much
    not much

---
+++ firebase1/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by aaronkuniansky-altman on 12/10/16.
+ */
+    // Initialize Firebase
+var config = {
+        apiKey: "AIzaSyC5_1TI2d_2M6DBgxn6UJHWTEYIReT2v-o",
+        authDomain: "classproject-49f02.firebaseapp.com",
+        databaseURL: "https://classproject-49f02.firebaseio.com",
+        storageBucket: "classproject-49f02.appspot.com",
+        messagingSenderId: "837229707786"
+    };
+firebase.initializeApp(config);
+
+alert("hi");
